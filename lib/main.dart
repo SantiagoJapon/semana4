13757852428f907840f61fd7d3993f0ff4c3834b
SantiagoJapon <sajaponpa@uide.edu.ec>
2023-12-14 18:24:48 -0500

    Titulo de un Hola

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+  
+}
